df89dcd979fd316388136b97ddb0cd5f9dfc0c04
Gengyuan Huang <gengyuan@ualberta.ca>
2023-11-25 13:46:37 -0700

    Setup dev

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'standard-with-typescript',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-}
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '',
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'jotai/babel/plugin-react-refresh',
+          'jotai/babel/plugin-debug-label',
+        ],
+      },
+    }),
+    splitVendorChunkPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@import '@/styles/index.scss';",
+      },
+    },
+  },
+  build: {
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1000,
+  },
+  define: {
+    // TODO: https://github.com/jotaijs/jotai-devtools/issues/82
+    // temp patch for jotai-devtools@0.6.0
+    'process.platform': null,
+  },
+});
